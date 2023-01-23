7113fbf7278de3bc85e1cd66c0035815f4df95fd
KodaiMeboso-Java <muxiguangda@gmail.com>
2023-01-23 13:31:33 +0900

    first commit

---
+++ src/test/java/com/example/RaiseTechTask6/RaiseTechTask6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RaiseTechTask6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaiseTechTask6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
