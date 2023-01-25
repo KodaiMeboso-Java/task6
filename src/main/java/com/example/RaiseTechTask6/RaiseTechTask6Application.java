ecce5307a7350806fb4d20f117848e06dcc38656
KodaiMeboso-Java <muxiguangda@gmail.com>
2023-01-25 14:19:32 +0900

    first time spring initializr changing

--- src/main/java/com/example/RaiseTechTask6/RaiseTechTask6Application.java
+++ src/main/java/com/example/RaiseTechTask6/RaiseTechTask6Application.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Struct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class RaiseTechTask6Application {
@@ -12,8 +18,10 @@ public class RaiseTechTask6Application {
 	public static void main(String[] args) {
 		SpringApplication.run(RaiseTechTask6Application.class, args);
 	}
+
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	public List hello(@RequestParam(value = "country", defaultValue = "World") String country) {
+		List<String> country_list = new ArrayList<>(Arrays.asList("Japan", "USA", "China", "UK"));
+		return country_list;
 	}
 }
